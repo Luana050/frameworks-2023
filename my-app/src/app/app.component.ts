3e4ea4c87e49e0c9ddc904b7aedca08838a3866e
Luana050 <luanaribeirosilva076@gmail.com>
2023-03-21 16:05:35 -0400

    funcao angular

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  nome = 'Luana'
+  count = 0;
+
+  sub(){
+    this.count--;
+ }
+
+  soma(){
+    this.count++;
+
+  }
 }
