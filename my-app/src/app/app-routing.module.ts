0291cd1b1a25662772fc89cd2c216e00eaf39022
Luana050 <127446320+Luana050@users.noreply.github.com>
2023-04-25 15:54:32 -0400

    Add files via upload

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
-import { NgModule } from '@angular/core';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { PagenotFoundComponent } from './component/pagenot-found/pagenot-found.component';
+import { GridComponent } from './component/grid/grid.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HeroComponent } from './componets/hero/hero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'hero' , component: HeroComponent},
+  {path: 'grid', component: GridComponent},
+  {path: 'dashboard', component: DashboardComponent},
+    
+
+
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '**', component: PagenotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { HeroComponent } from './componets/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { GridComponent } from './component/grid/grid.component';
+import { PagenotFoundComponent } from './component/pagenot-found/pagenot-found.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    LoginComponent
+    LoginComponent,
+    GridComponent,
+    PagenotFoundComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
