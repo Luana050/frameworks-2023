25ec0fa4ca8ef9fb136ceb30b38109117bafc2f8
vxizza <izadoraisidoro145@gmail.com>
2023-06-06 14:25:11 -0400

    forms

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HeroComponent } from './componets/hero/hero.component';
+import { TaskComponent } from './components/task/task.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'hero' , component: HeroComponent},
   {path: 'grid', component: GridComponent},
+  {path: 'task', component: TaskComponent },
   {path: 'dashboard', component: DashboardComponent},
     
 
